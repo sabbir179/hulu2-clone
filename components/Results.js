c10bd4678acbc69b3a94fddcd7b75fc42e396f64
Sabbir Ahmed <sabbirahmed@Sabbirs-MacBook-Air.local>
2021-07-02 20:05:04 +0600

    hulu clone in next js , tailwind css

---
+++ components/Results.js
@@ -0,0 +1,9 @@
+function Results() {
+    return (
+        <div>
+            this is resulst 
+        </div>
+    )
+}
+
+export default Results
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 
 import Header from '../components/Header'
 import Nav from '../components/Nav'
+import Results from '../components/Results'
 
 
 export default function Home() {
@@ -13,13 +14,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header */}
       <Header/>
-
-      {/* Nav */}
-
       <Nav />
       {/* Results */}
+      <Results />
     </div>
   )
 }
